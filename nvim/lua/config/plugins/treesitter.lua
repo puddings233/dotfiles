39c57d9079ab22955c5ed4b6c1fc8c253c7489b0
puddings233 <xiefanko@gmail.com>
2024-01-24 06:17:11 +0800

    fix markdown.

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return{
 	"nvim-treesitter/nvim-treesitter",
 	--need package "tree-sitter-cli" installed.
-	build = {":TSInstall c lua vim vimdoc query python markdown"},
+	ft={"lua", "python", "sh"},
+	build = {":TSInstall c lua vim vimdoc query python markdown_inline bash"},
 	config = function ()
 		require'nvim-treesitter.configs'.setup {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "markdown" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", " markdown_inline", "bash" },
 			 auto_install = true,
 			 highlight = {
 				 enable = true,
