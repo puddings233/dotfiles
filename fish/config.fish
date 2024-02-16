55fd350e3c84dd2133e06f0a4736eb123bacbe70
puddings233 <puddings233@outlook.com>
2024-02-16 17:52:54 +0800

    fix fish cursor wrong when using insert mod

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx EDITOR nvim
+    set -gx fish_cursor_insert line
     fish_vi_key_bindings 
     set fish_key_bindings fish_user_key_bindings
 end
