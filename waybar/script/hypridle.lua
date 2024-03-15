1d363ad60175e984b896d34edb2d020395210aba
puddings233 <puddings233@outlook.com>
2024-03-15 18:22:50 +0800

    hypridle.lua add a script that help me to control idle daemon manually.

---
+++ waybar/script/hypridle.lua
@@ -0,0 +1,28 @@
+#!/usr/bin/lua
+local status = os.execute("pgrep hypridle >> /dev/null")
+
+local function check_status()
+	if status ~= nil then
+		print("d")
+	else
+		print("d")
+	end
+end
+
+local function switch_status()
+	if status ~= nil then
+	os.execute("killall hypridle")
+	else
+	os.execute("hypridle &")
+	end
+end
+
+function Run_hypridle_lua()
+	if arg[1] == "check" then
+		check_status()
+	elseif arg[1] == "switch" then
+		switch_status()
+ end
+end
+
+Run_hypridle_lua()
