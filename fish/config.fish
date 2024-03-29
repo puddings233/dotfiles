dc3510eda73b92f67d4ee1bf38a8e0d89ac6c852
puddings233 <puddings233@outlook.com>
2024-03-29 16:53:45 +0800

    gpg-agent

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx EDITOR nvim
+    set -e SSH_AGENT_PID
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
     set -gx fish_cursor_insert line
     fish_vi_key_bindings 
     set fish_key_bindings fish_user_key_bindings
