2aaeeb836a6d84f9a18586011d5120f9877bba69
puddings233 <121273557+puddings233@users.noreply.github.com>
2024-04-23 14:15:51 +0800

    Update treesitter.lua (#20)
    edit online, hope it was good

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,8 @@ return{
 	"nvim-treesitter/nvim-treesitter",
 	-- need package "tree-sitter-cli" installed.
 	lazy = false,
+	build = ":TSUpdate",
 	config = function ()
-
 		require'nvim-treesitter.configs'.setup {
 			ensure_installed = "all",
 			sync_install = true,
