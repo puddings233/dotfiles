79c74698ded9192856d4116df662a6c37e18b343
puddings233 <121273557+puddings233@users.noreply.github.com>
2024-04-23 16:58:59 +0800

    Change for nvim-nightly (#21)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,8 +6,8 @@ return{
 	config = function ()
 		require'nvim-treesitter.configs'.setup {
 			ensure_installed = "all",
-			sync_install = true,
-			auto_install = false,
+			sync_install = false,
+			auto_install = true,
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = true,
